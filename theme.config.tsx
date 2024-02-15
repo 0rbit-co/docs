9d9bfcf8dcf8d968b65c2f499aefbc08a47317fa
megabyte0x <yashygkf@gmail.com>
2024-02-15 22:07:16 +0530

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>0rbit</span>,
+  project: {
+    link: 'https://github.com/0rbit-co',
+  },
+  chat: {
+    link: 'https://discord.gg/mQg3kqgXDk',
+  },
+  docsRepositoryBase: 'https://github.com/0rbit-co/docs/blob/main',
+  footer: {
+    text: '0rbit Developer Docs',
+  },
+}
+
+export default config
